308d0850b18ca5b5ea93c2da89690a57cacba2b8
evrengokduman <sukruevrengokduman@gmail.com>
2023-05-31 23:49:36 +0300

    finished functionality

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach((sound) => {
-  const btn = document.createElement('button')
-  btn.classList.add('btn')
-  btn.innerText = sound
-  document.getElementById('buttons').appendChild(btn)
+  //loop through each sound in the array
+  const btn = document.createElement('button') // create a button element
+
+  btn.classList.add('btn') // add btn class to the button element
+
+  btn.innerText = sound // add the sound name to the button
+
+  btn.addEventListener('click', () => {
+    stopSongs() // click event starts by stopping all the sounds, so if something is playing, it will stop. By clicking something we also start another audio file.
+
+    document.getElementById(sound).play() // play the sound when the button is clicked
+  })
+  document.getElementById('buttons').appendChild(btn) // appendChild() adds a node to the end of the list of children of a specified parent node.
 })
+
+function stopSongs() {
+  // stop the sound when the button is clicked
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0
+  })
+}
